51bb07d303a8b989fc71dd565b049d4312c527bb
Thayna Santana <thaynamenezes01@gmail.com>
2019-07-18 19:11:37 -0400

    please work

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,14 +25,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `data`,
-        path: `${__dirname}/src/data/`,
-        ignore: [`**/\.*`], // ignore files starting with a dot
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         name: `pages`,
         path: `${__dirname}/src/pages/`,
       },
